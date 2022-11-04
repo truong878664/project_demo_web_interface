3fe76f227f6284482bccb3bdda6abc583973e64f
Thao <machhmthao02@gmail.com>
2022-11-04 11:18:01 +0700

    duongDan

--- resources/views/frontend/layouts/home.blade.php
+++ resources/views/frontend/layouts/home.blade.php
@@ -47,18 +47,5 @@
         </tr>
         </tbody>
     </table>  
-            <td>john@example.com</td>
-        </tr>
-        <tr>
-            <td>Mary</td>
-            <td>Moe</td>
-            <td>mary@example.com</td>
-        </tr>
-        <tr>
-            <td>July</td>
-            <td>Dooley</td>
-            <td>july@example.com</td>
-        </tr>
-        </tbody>
-    </table>    
-@stop
+ 
+@endsection
