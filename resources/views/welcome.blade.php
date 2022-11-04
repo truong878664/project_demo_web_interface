238c67c331e1f7103a3799069db4c5582d75bc18
Thao <machhmthao02@gmail.com>
2022-11-04 09:14:31 +0700

    ..

--- resources/views/welcome.blade.php
+++ resources/views/welcome.blade.php
@@ -2,25 +2,25 @@
 <!DOCTYPE html>
 <html lang="en">
 <head>
-  <title>Demo</title>
-  <meta charset="utf-8">
-  <meta name="viewport" content="width=device-width, initial-scale=1">
-  <link rel="stylesheet" href="https://maxcdn.bootstrapcdn.com/bootstrap/4.4.1/css/bootstrap.min.css">
-  <script src="https://maxcdn.bootstrapcdn.com/bootstrap/4.4.1/js/bootstrap.min.js"></script>
-  <link rel="stylesheet" href="main.css">
-  <style>
+    <title>Demo</title>
+    <meta charset="utf-8">
+    <meta name="viewport" content="width=device-width, initial-scale=1">
+    <link rel="stylesheet" href="https://maxcdn.bootstrapcdn.com/bootstrap/4.4.1/css/bootstrap.min.css">
+    <script src="https://maxcdn.bootstrapcdn.com/bootstrap/4.4.1/js/bootstrap.min.js"></script>
+    <link rel="stylesheet" href="main.css">
+    <style>
   .fakeimg {
     height: 200px;
-    background: #aaa;
+    background: #3a79de;
   }
-  </style>
+    </style>
 </head>
 <body>
  
     <div class="container">
     
-        <div class="jumbotron text-center" style="margin-bottom:0">
-            <h1>Trang Demo </h1>
+        <div class="jumbotron text-center header" style="margin-bottom:0">
+            <h1>Demo robot</h1>
         </div>
 
         <nav class="navbar navbar-expand-sm bg-dark navbar-dark">
@@ -31,13 +31,13 @@
         <div class="collapse navbar-collapse" id="collapsibleNavbar">
             <ul class="navbar-nav">
             <li class="nav-item">
-                <a class="nav-link" href="#">Mục 1</a>
+                <a class="nav-link" href="#">Navigation</a>
             </li>
             <li class="nav-item">
-                <a class="nav-link" href="#">Mục 2</a>
+                <a class="nav-link" href="#">Mission</a>
             </li>
             <li class="nav-item">
-                <a class="nav-link" href="#">Mục 3</a>
+                <a class="nav-link" href="#">control</a>
             </li>    
             </ul>
         </div>  
@@ -50,7 +50,9 @@
                 <p>Danh sach menu</p>
                 <ul class="nav nav-pills flex-column">
                     <li class="nav-item">
-                        <a class="nav-link" href="#">A</a>
+                    <li class="nav-item">
+                        <input type="file" name="image_product" ></th>
+                    </li>
                     </li>
                     <li class="nav-item">
                         <a class="nav-link" href="#">B</a>
